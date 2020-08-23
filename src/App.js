9fab8ef3564e329b79ab63c02742a996250b7121
wladm <wladmyr@visiondevelopment.co>
2020-08-23 00:37:52 -0300

    Repositories Web Challenge Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Desafio Node.js",
+      url: "https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: [
+        "Node.js",
+        "Flutter",
+        "React Native"
+      ],
+      likes: 0
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    //Não usa response pois não retorna nada!
+    await api.delete(`repositories/${id}`);
+
+    //Mantendo(Filtrando) apenas nos repositórios, os repositórios que tem IDS diferentes do qual foi removido;
+    setRepositories(repositories.filter(
+      repo => repo.id !== id
+    ));
   }
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
+
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
